efdf30f2c71d02734b83dd158c4202a2a66bb8c5
Saurabh Satish Dashpute <128281040+Saurabh9105@users.noreply.github.com>
2023-04-21 11:02:13 +0530

    Add files via upload

---
+++ question7pattern1.java
@@ -0,0 +1,19 @@
+package assignment2;
+
+public class question7pattern1 {
+
+	public static void main(String[] args) {
+int i;
+int j;
+for(i=1;i<=4;i++)
+{
+	for(j=1;j<=10;j++)
+		{
+		System.out.print("*");
+		}
+	System.out.println();
+		}
+}
+	}
+
+
