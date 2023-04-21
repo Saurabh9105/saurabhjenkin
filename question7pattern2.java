df7a12def039dde31feed6125cb83ea5385e3c24
Saurabh9105 <saurabhdashpute9105@gmail.com>
2023-04-21 11:04:43 +0530

    new

---
+++ question7pattern2.java
@@ -0,0 +1,17 @@
+package assignment2;
+
+public class question7pattern2 {
+
+	public static void main(String[] args) {
+		int i;
+		int j;
+		for(i=1;i<=5;i++)
+		{
+          for(j=1;j<=i;j++)
+          {
+        	System.out.print("*");  
+	}
+          System.out.println();
+	}
+}
+}
